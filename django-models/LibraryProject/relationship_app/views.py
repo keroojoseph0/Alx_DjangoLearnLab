2ac4c7b8c09a6137717219233f9c75542f7a1996
keroojoseph0 <keroojoseph0@gmail.com>
2025-08-05 19:07:54 +0300

    Modified relationship app view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .utils import is_admin, is_librarian, is_member
 from .forms import BookForm
 
