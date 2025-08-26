aa10b09c7dfc6749452311f3b3b0e19e9a50822b
keroojoseph0 <keroojoseph0@gmail.com>
2025-08-26 20:21:34 +0300

    modify serialize file

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -2,7 +2,7 @@ from django.forms import fields
 from rest_framework import serializers
 from .models import Book
 
-class BookSerializer(serializers.Serializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         models = Book
         fields = '__all__'
