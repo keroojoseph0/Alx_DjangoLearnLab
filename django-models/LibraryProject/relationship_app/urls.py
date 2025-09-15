b58951e47d41f7966657e44d37ec24cbe62aeb3b
keroojoseph0 <keroojoseph0@gmail.com>
2025-09-15 14:41:59 +0300

    Add auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('booklist', list_books, name='booklist'),
     path('books', BookList.as_view(), name='books'),
     path('library', LibraryDetailView.as_view(), name='library'),
-    path('signup', views.regiser, name='signup'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name = 'registration/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
