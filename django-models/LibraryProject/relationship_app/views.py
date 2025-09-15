2bdbc6c3c78ab3ca1daf9b9223dde909126d189e
keroojoseph0 <keroojoseph0@gmail.com>
2025-09-15 13:00:07 +0300

    Import library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Book, Library
-from django.views.generic import ListView, DetailView
+from .models import Book
+from .models import Library
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 
 # Create your views here.
