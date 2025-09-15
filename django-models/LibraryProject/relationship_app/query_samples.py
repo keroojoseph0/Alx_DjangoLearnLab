8b84a1b3b8ed9fc0b358e1659d6e78eba187321e
keroojoseph0 <keroojoseph0@gmail.com>
2025-09-15 11:57:44 +0300

    Apply queryset on models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,27 @@
 from .models import Book, Librarian, Library, Author
 
-authors = Author.objects.all()
-book_by_auher = authors.books.alll()
+# 1. Query all books by a specific author
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        return author.books.all()  # thanks to related_name="books"
+    except Author.DoesNotExist:
+        return []
 
-librarys = Library.objects.all()
-book_in_library = librarys.books.all()
+
+# 2. List all books in a library
+def get_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()  # ManyToManyField -> use .all()
+    except Library.DoesNotExist:
+        return []
 
 
-librarian_in_library = librarys.librarian
+# 3. Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian  # OneToOneField -> direct relation
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
