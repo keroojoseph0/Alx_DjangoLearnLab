ca6afc5afc6799edf09146312b66bc664863bac6
Keroles Joseph <143596572+keroojoseph0@users.noreply.github.com>
2025-09-16 18:38:16 +0300

    Delete advanced_features_and_security/LibraryProject directory

--- advanced_features_and_security/LibraryProject/bookshelf/utlis.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import Group
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-user = User.objects.get(username='admin')
-group, created = Group.objects.get_or_create(name='Admins')
-
-group.user_set.add(user)
